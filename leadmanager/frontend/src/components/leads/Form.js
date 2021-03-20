6d6a7fb993f3abf8342f06b0e0b7ad8721230857
Qasim Hafeez <qasimh556@gmail.com>
2021-03-20 17:14:56 +0400

    Add Lead Form

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,9 +1,75 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLead } from "../../actions/leads";
 
 export class Form extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  static propTypes = {
+    addLead: PropTypes.func.isRequired,
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
+  };
+
   render() {
-    return <div>Add Form</div>;
+    const { name, email, message } = this.state;
+    return (
+      <Fragment>
+        <div className="card card-body mt-4 mb-4">
+          <h2>Add Lead</h2>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label>Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                onChange={this.onChange}
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                onChange={this.onChange}
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label>Message</label>
+              <input
+                type="text"
+                className="form-control"
+                name="message"
+                onChange={this.onChange}
+                value={message}
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </Fragment>
+    );
   }
 }
 
-export default Form;
+export default connect(null, { addLead })(Form);
